4861959e21c6f6311105394b6db331dc28c8f4f0
Orlando <lopezorlando23@gmail.com>
2021-01-06 10:38:57 -0700

    Create OperationsController.cs
    Created the OperationsController and added the reload method

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class OperationsController : ControllerBase
+
+    {
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError)
+;            }
+
+            
+        }
+    }
+
+}
